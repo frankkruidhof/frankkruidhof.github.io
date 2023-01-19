16d377c2ed11cc06eb27d938303c975e3104f14c
frank kruidhof <frank.kruidhof@mailbox.org>
2023-01-19 22:08:06 +0100

    ok, works, thanks dionyziz

--- js/render.js
+++ js/render.js
@@ -2,16 +2,20 @@ var canvas = document.getElementsByTagName( 'canvas' )[ 0 ];
 var ctx = canvas.getContext( '2d' );
 var W = 300, H = 600;
 var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
+const IbcImg = new Image(); // Create new img element
+IbcImg.src = "image/ibc.png"; // Set source path
+const BackgroundImg = new Image();
+BackgroundImg.src = "image/terrein.png";
 
 // draw a single square at (x, y)
 function drawBlock( x, y ) {
-    ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
-    ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+    ctx.drawImage(IbcImg, BLOCK_W * x - 8, BLOCK_H * y - 8, BLOCK_W * 1.8, BLOCK_H * 1.8);
 }
 
 // draws the board and the moving shape
 function render() {
     ctx.clearRect( 0, 0, W, H );
+    ctx.drawImage(BackgroundImg, 0, 0);
 
     ctx.strokeStyle = 'black';
     for ( var x = 0; x < COLS; ++x ) {
